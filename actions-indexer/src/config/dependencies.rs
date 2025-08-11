9cdc53b47454ec911472a793bdc49368e7d75872
0xjagger <194556652+0xJagger@users.noreply.github.com>
2025-08-11 18:48:54 -0300

    refactor: update loader type in Dependencies struct to use PostgresActionsRepository

--- actions-indexer/src/config/dependencies.rs
+++ actions-indexer/src/config/dependencies.rs
@@ -1,12 +1,12 @@
 use actions_indexer_pipeline::consumer::ConsumeActions;
-use actions_indexer_pipeline::loader::ActionsRepository;
+use actions_indexer_pipeline::loader::PostgresActionsRepository;
 use actions_indexer_pipeline::processor::ProcessActions;
 use crate::errors::IndexingError;
 
 pub struct Dependencies {
     pub consumer: Box<dyn ConsumeActions>,
     pub processor: Box<dyn ProcessActions>,
-    pub loader: Box<dyn ActionsRepository<sqlx::Postgres>>,
+    pub loader: Box<PostgresActionsRepository>,
 }
 
 impl Dependencies {
