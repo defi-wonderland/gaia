3bb07acbd948b02cdea820cda81a25628ef165ca
0xjagger <194556652+0xJagger@users.noreply.github.com>
2025-08-11 16:24:35 -0300

    chore: replace futures crate with tokio_streams

--- actions-indexer-consumer/src/lib.rs
+++ actions-indexer-consumer/src/lib.rs
@@ -3,7 +3,7 @@ mod errors;
 use actions_indexer_types::models::ActionEvent;
 use crate::errors::ConsumerError;
 
-use futures03::Stream;
+use tokio_stream::Stream;
 use std::pin::Pin;
 
 pub trait ConsumeActions {
