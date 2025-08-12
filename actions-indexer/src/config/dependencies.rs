f22e33dbbb24082206656565036990d2c2ea3309
0xjagger <194556652+0xJagger@users.noreply.github.com>
2025-08-12 16:20:03 -0300

    refactor: update Dependencies struct

--- actions-indexer/src/config/dependencies.rs
+++ actions-indexer/src/config/dependencies.rs
@@ -1,12 +1,12 @@
 use actions_indexer_pipeline::consumer::ConsumeActions;
-use actions_indexer_pipeline::loader::PostgresActionsRepository;
+use actions_indexer_pipeline::loader::ActionsLoader;
 use actions_indexer_pipeline::processor::ProcessActions;
 use crate::errors::IndexingError;
 
 pub struct Dependencies {
     pub consumer: Box<dyn ConsumeActions>,
     pub processor: Box<dyn ProcessActions>,
-    pub loader: Box<PostgresActionsRepository>,
+    pub loader: Box<ActionsLoader>,
 }
 
 impl Dependencies {
--- actions-indexer/src/errors/mod.rs
+++ actions-indexer/src/errors/mod.rs
@@ -1,4 +1,5 @@
 #[derive(Debug, thiserror::Error)]
 pub enum IndexingError {
-    // TODO: Add errors
+    #[error("Orchestrator error: {0}")]
+    Orchestrator(#[from] actions_indexer_pipeline::errors::OrchestratorError),
 }
