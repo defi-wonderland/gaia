d99f2d779dd82bef8626504339298e738659bfdc
0xjagger <194556652+0xJagger@users.noreply.github.com>
2025-08-13 12:21:59 -0300

    feat: implement ActionsProcessor for processing ActionEvent data

--- actions-indexer-pipeline/src/processor/mod.rs
+++ actions-indexer-pipeline/src/processor/mod.rs
@@ -1,18 +1,21 @@
 //! This module defines the `ProcessActions` trait for processing raw action events.
 //! It provides an interface for transforming `ActionEvent` data into structured `Action` data.
-use actions_indexer_shared::types::{ActionEvent, Action};
 use crate::errors::ProcessorError;
+use actions_indexer_shared::types::{ActionEvent, Action};
+
+mod actions_processor;
+
+pub use actions_processor::ActionsProcessor;
 
 /// Defines the interface for processing raw `ActionEvent` data into structured `Action` data.
 ///
 /// Implementors of this trait are responsible for applying business logic and transformations
 /// to the incoming action events.
 pub trait ProcessActions {
-    /// Processes a slice of `ActionEvent`s and returns a `Result` containing a vector of `Action`s.
+    /// Processes a slice of `ActionEvent`s and returns a vector of `Action`s.
     ///
     /// This method takes an array of raw `ActionEvent`s, applies necessary processing rules,
-    /// and converts them into a structured `Action` format. It returns a `Result` that is
-    /// `Ok(Vec<Action>)` on successful processing or a `ProcessorError` if an error occurs.
+    /// and converts them into a structured `Action` format. It returns a `Vec<Action>` on successful processing.
     ///
     /// # Arguments
     ///
@@ -20,7 +23,26 @@ pub trait ProcessActions {
     ///
     /// # Returns
     ///
-    /// A `Result` which is `Ok(Vec<Action>)` on successful processing or a
-    /// `ProcessorError` if an error occurs during processing.
-    fn process(&self, actions: &[ActionEvent]) -> Result<Vec<Action>, ProcessorError>;
+    /// A `Vec<Action>` on successful processing.
+    fn process(&self, actions: &[ActionEvent]) -> Vec<Action>;
+}
+
+/// Defines the interface for handling a single `ActionEvent` and converting it into a structured `Action` data.
+///
+/// Implementors of this trait are responsible for applying business logic and transformations
+/// to the incoming action event.
+pub trait HandleAction: Send + Sync {
+    /// Handles a single `ActionEvent` and converts it into a structured `Action` data.
+    ///
+    /// This method takes an `ActionEvent` and applies necessary business logic and transformations
+    /// to convert it into a structured `Action` format. It returns a `Result` containing the processed `Action` on successful processing or a `ProcessorError` if an error occurs.
+    ///
+    /// # Arguments
+    ///
+    /// * `action` - The `ActionEvent` to be processed.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the processed `Action` on successful processing or a `ProcessorError` if an error occurs.
+    fn handle(&self, action: &ActionEvent) -> Result<Action, ProcessorError>;
 }
