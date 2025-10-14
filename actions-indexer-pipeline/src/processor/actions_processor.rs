3053a964ba14f525749604000e69c95956c2ad1a
0xjagger <194556652+0xJagger@users.noreply.github.com>
2025-10-14 13:38:42 -0300

    feat: update action type handling to use ActionType enum for votes

--- actions-indexer-pipeline/src/processor/actions_processor.rs
+++ actions-indexer-pipeline/src/processor/actions_processor.rs
@@ -68,7 +68,7 @@ mod tests {
 
     use crate::errors::ProcessorError;
     use crate::processor::{ActionsProcessor, HandleAction, ProcessActions};
-    use actions_indexer_shared::types::{Action, ActionRaw, Vote, VoteValue};
+    use actions_indexer_shared::types::{Action, ActionRaw, Vote, VoteValue, ActionType, ObjectType};
     use alloy::hex::FromHex;
     use alloy::primitives::{Address, Bytes, TxHash};
     use uuid::uuid;
@@ -92,7 +92,7 @@ mod tests {
     fn make_action_event(payload_byte: u8) -> ActionRaw {
         ActionRaw {
             sender: Address::from_hex("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045").unwrap(),
-            action_type: 0,
+            action_type: ActionType::Vote,
             action_version: 1,
             space_pov: uuid!("e50fe85c-108a-4d4a-97b9-376a1e5d318b"),
             object_id: uuid!("a7ef0016-a2f4-44fb-82ca-a4f5c61d2cf5"),
@@ -120,7 +120,7 @@ mod tests {
 
     fn mocked_processor() -> ActionsProcessor {
         let mut processor = ActionsProcessor::new();
-        processor.register_handler(1, 0, 0, Arc::new(MockHandler));
+        processor.register_handler(1, ActionType::Vote, ObjectType::Entity, Arc::new(MockHandler));
         processor
     }
 
@@ -192,7 +192,7 @@ mod tests {
         let processor = mocked_processor();
         let action_event = ActionRaw {
             sender: Address::from_hex("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045").unwrap(),
-            action_type: 2, // no handler defined for this action type
+            action_type: ActionType::Vote,
             action_version: 1,
             space_pov: uuid!("e50fe85c-108a-4d4a-97b9-376a1e5d318b"),
             object_id: uuid!("a7ef0016-a2f4-44fb-82ca-a4f5c61d2cf5"),
