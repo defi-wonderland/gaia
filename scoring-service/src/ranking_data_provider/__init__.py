ca7c765f3ae2e76593fd0882b300bc991bf69954
0xjagger <194556652+0xJagger@users.noreply.github.com>
2025-12-11 13:36:39 -0300

    feat: add RankingDataProvider

---
+++ scoring-service/src/ranking_data_provider/__init__.py
@@ -0,0 +1,5 @@
+"""Ranking data provider module for fetching ranking data from PostgreSQL."""
+
+from .ranking_data_provider import RankingDataProvider, RankingData
+
+__all__ = ["RankingDataProvider", "RankingData"]
