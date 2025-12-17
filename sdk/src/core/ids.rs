3381a37f75417f3914d1e4c2dd584fbd9ad44398
Neo <neo@wonderland.xyz>
2025-12-17 07:09:33 -0800

    fix(sdk): uuids for entity property ids
    Co-authored-by: Jagger <194556652+0xJagger@users.noreply.github.com>
    Signed-off-by: Neo <neo@wonderland.xyz>

--- sdk/src/core/ids.rs
+++ sdk/src/core/ids.rs
@@ -1,3 +1,3 @@
-pub const NAME_ATTRIBUTE: &str = "LuBWqZAu6pz54eiJS5mLv8";
-pub const DESCRIPTION_ATTRIBUTE: &str = "LA1DqP5v6QAdsgLPXGF3YA";
-pub const AVATAR_ATTRIBUTE: &str = "399xP4sGWSoepxeEnp3UdR";
+pub const NAME_ATTRIBUTE: &str = "a126ca53-0c8e-48d5-b888-82c734c38935";
+pub const DESCRIPTION_ATTRIBUTE: &str = "9b1f76ff-9711-404c-861e-59dc3fa7d037";
+pub const AVATAR_ATTRIBUTE: &str = "9b1f76ff-9711-404c-861e-59dc3fa7d037";
